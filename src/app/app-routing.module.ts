0657a79c160dd491e7fa42a617fdce90f475b00d
SamuelCano03 <U202116508@upc.edu.pe>
2024-06-21 15:25:31 -0500

    Estructura inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, { path: 'vet', loadChildren: () => import('./vet/vet.module').then(m => m.VetModule) }, { path: 'petowner', loadChildren: () => import('./petowner/petowner.module').then(m => m.PetownerModule) }, { path: 'community', loadChildren: () => import('./community/community.module').then(m => m.CommunityModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/petowner/petowner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-petowner',
+  templateUrl: './petowner.component.html',
+  styleUrl: './petowner.component.css'
+})
+export class PetownerComponent {
+
+}
